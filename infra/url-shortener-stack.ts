fbcc15313f2c6e6f949abe60695cc9191ecc1197
alan.price <alan@inicion.com>
2024-11-08 15:17:16 -0600

    Started into making this a platform.

--- infra/url-shortener-stack.ts
+++ infra/url-shortener-stack.ts
@@ -1,24 +1,63 @@
 import * as cdk from 'aws-cdk-lib';
-import * as lambda from 'aws-cdk-lib/aws-lambda';
+import * as cognito from 'aws-cdk-lib/aws-cognito';
+import * as s3 from 'aws-cdk-lib/aws-s3';
+import * as cloudfront from 'aws-cdk-lib/aws-cloudfront';
+import * as cloudfrontOrigins from 'aws-cdk-lib/aws-cloudfront-origins';
 import * as apigateway from 'aws-cdk-lib/aws-apigateway';
+import * as lambda from 'aws-cdk-lib/aws-lambda';
+import * as dynamodb from 'aws-cdk-lib/aws-dynamodb';
 import * as route53 from 'aws-cdk-lib/aws-route53';
 import * as targets from 'aws-cdk-lib/aws-route53-targets';
 import * as acm from 'aws-cdk-lib/aws-certificatemanager';
-import * as dynamodb from 'aws-cdk-lib/aws-dynamodb';
 import * as path from 'path';
 
-interface URLShortenerStackProps extends cdk.StackProps {
-    tableName: string;
-    domainName: string;
-}
-
 export class URLShortenerStack extends cdk.Stack {
-    constructor(scope: cdk.App, id: string, props: URLShortenerStackProps) {
+    constructor(scope: cdk.App, id: string, props?: cdk.StackProps) {
         super(scope, id, props);
 
         const tableName = this.node.tryGetContext('tableName') || props.tableName;
         const domainName = this.node.tryGetContext('domainName') || props.domainName;
         const region = this.node.tryGetContext('region') || props.env?.region;
+        const subdomain = 'app';
+
+        // Cognito User Pool
+        const userPool = new cognito.UserPool(this, 'UserPool', {
+            selfSignUpEnabled: true,
+            signInAliases: { email: true },
+            autoVerify: { email: true },
+        });
+
+        const userPoolClient = new cognito.UserPoolClient(this, 'UserPoolClient', {
+            userPool,
+        });
+
+        // S3 Bucket for Static Site
+        const siteBucket = new s3.Bucket(this, 'SiteBucket', {
+            websiteIndexDocument: 'index.html',
+            publicReadAccess: true,
+            removalPolicy: cdk.RemovalPolicy.DESTROY,
+        });
+
+        // CloudFront Distribution for Static Site
+        const distribution = new cloudfront.Distribution(this, 'SiteDistribution', {
+            defaultBehavior: { origin: new cloudfrontOrigins.S3Origin(siteBucket) },
+            domainNames: [`${subdomain}.${domainName}`],
+            certificate: new acm.Certificate(this, 'SiteCertificate', {
+                domainName: `${subdomain}.${domainName}`,
+                validation: acm.CertificateValidation.fromDns(),
+            }),
+        });
+
+        // Route 53 Alias Record for CloudFront
+        const hostedZone = route53.HostedZone.fromLookup(this, 'HostedZone', {
+            domainName,
+        });
+
+        new route53.ARecord(this, 'SiteAliasRecord', {
+            zone: hostedZone,
+            target: route53.RecordTarget.fromAlias(new targets.CloudFrontTarget(distribution)),
+            recordName: subdomain,
+        });
 
         // DynamoDB Table
         const table = new dynamodb.Table(this, 'ShortcodesTable', {
@@ -37,80 +76,62 @@ export class URLShortenerStack extends cdk.Stack {
             environment: {
                 DYNAMODB_TABLE_NAME: table.tableName,
                 REGION: region,
+                USER_POOL_ID: userPool.userPoolId,
+                USER_POOL_CLIENT_ID: userPoolClient.userPoolClientId,
             },
         });
 
-        // Grant the Lambda function permissions to access DynamoDB
-        table.grantReadWriteData(urlShortenerLambda);
+        table.grantReadWriteData(shortcodesLambda);
 
         // API Gateway
-        const api = new apigateway.LambdaRestApi(this, 'UrlShortenerApi', {
-            handler: urlShortenerLambda,
-            proxy: false,
+        const api = new apigateway.RestApi(this, 'ShortcodesApi', {
+            restApiName: 'Shortcodes Service',
+            description: 'This service handles shortcodes.',
         });
 
-        const shortcodeResource = api.root.addResource('s');
-        shortcodeResource.addResource('{code}').addMethod('GET', new apigateway.LambdaIntegration(urlShortenerLambda), {
-            apiKeyRequired: false
+        const authorizer = new apigateway.CognitoUserPoolsAuthorizer(this, 'Authorizer', {
+            cognitoUserPools: [userPool],
         });
 
-        // Add a default ANY method to the root resource
-        api.root.addMethod('ANY');
-
-        // Add a new resource and method for generating shortcodes
-        const generateResource = api.root.addResource('generate');
-        const generateMethod = generateResource.addMethod('POST', new apigateway.LambdaIntegration(urlShortenerLambda), {
-            apiKeyRequired: true,
+        const shortcodes = api.root.addResource('shortcodes');
+        shortcodes.addMethod('GET', new apigateway.LambdaIntegration(shortcodesLambda), {
+            authorizer,
+            authorizationType: apigateway.AuthorizationType.COGNITO,
+        });
+        shortcodes.addMethod('POST', new apigateway.LambdaIntegration(shortcodesLambda), {
+            authorizer,
+            authorizationType: apigateway.AuthorizationType.COGNITO,
         });
 
-        // Create an API key and usage plan
-        const apiKey = api.addApiKey('ApiKey');
-        const plan = api.addUsagePlan('UsagePlan', {
-            name: 'Easy',
-            throttle: {
-                rateLimit: 10,
-                burstLimit: 2,
-            },
+        const shortcode = shortcodes.addResource('{code}');
+        shortcode.addMethod('GET', new apigateway.LambdaIntegration(shortcodesLambda), {
+            authorizer,
+            authorizationType: apigateway.AuthorizationType.COGNITO,
         });
-        plan.addApiKey(apiKey);
-        plan.addApiStage({
-            stage: api.deploymentStage,
-            throttle: [
-                {
-                    method: generateMethod,
-                    throttle: {
-                        rateLimit: 10,
-                        burstLimit: 2,
-                    },
-                },
-            ],
+        shortcode.addMethod('PUT', new apigateway.LambdaIntegration(shortcodesLambda), {
+            authorizer,
+            authorizationType: apigateway.AuthorizationType.COGNITO,
+        });
+        shortcode.addMethod('DELETE', new apigateway.LambdaIntegration(shortcodesLambda), {
+            authorizer,
+            authorizationType: apigateway.AuthorizationType.COGNITO,
         });
 
-        // Route 53 Hosted Zone Lookup
-        const hostedZone = route53.HostedZone.fromLookup(this, 'HostedZone', {
-            domainName: domainName,
+        const metrics = api.root.addResource('metrics');
+        metrics.addMethod('GET', new apigateway.LambdaIntegration(shortcodesLambda), {
+            authorizer,
+            authorizationType: apigateway.AuthorizationType.COGNITO,
         });
 
-        // Request an SSL Certificate for the custom domain
-        const certificate = new acm.Certificate(this, 'ApiGatewayCertificate', {
-            domainName: domainName, // Use the domain name for HTTPS
-            validation: acm.CertificateValidation.fromDns(hostedZone),
+        const metric = metrics.addResource('{code}');
+        metric.addMethod('GET', new apigateway.LambdaIntegration(shortcodesLambda), {
+            authorizer,
+            authorizationType: apigateway.AuthorizationType.COGNITO,
         });
 
-        // Create a custom domain for API Gateway
-        const customDomain = new apigateway.DomainName(this, 'CustomDomain', {
-            domainName: domainName,
-            certificate: certificate,
-            endpointType: apigateway.EndpointType.REGIONAL,
-        });
-
-        // Map the custom domain to the API Gateway stage
-        customDomain.addBasePathMapping(api, { basePath: '' });
-
-        // Route 53 Alias Record for the custom domain
-        new route53.ARecord(this, 'ApiGatewayAliasRecord', {
-            zone: hostedZone,
-            target: route53.RecordTarget.fromAlias(new targets.ApiGatewayDomain(customDomain)),
-        });
+        // Public Access to Shortcodes
+        const publicShortcodes = api.root.addResource('s');
+        const publicShortcode = publicShortcodes.addResource('{code}');
+        publicShortcode.addMethod('GET', new apigateway.LambdaIntegration(shortcodesLambda));
     }
 }
